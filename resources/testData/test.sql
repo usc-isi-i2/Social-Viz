83a87b94786079e15edcd50a364274da74f8bbbc
Vaishnavi Dalvi <dalvi.vaishnavi90@gmail.com>
2013-12-18 21:56:45 -0800

    Final package

--- resources/testData/test.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE health_tweets_test(entities_hashtags VARCHAR(2000), created_at date, user_id double, user_screen_name varchar(50));
-
-LOAD DATA INFILE 'C:/Temp/test.csv' INTO TABLE health_tweets_test
-  FIELDS TERMINATED BY ',' ENCLOSED BY '"'
-  LINES TERMINATED BY '\n'
-  IGNORE 1 LINES;
